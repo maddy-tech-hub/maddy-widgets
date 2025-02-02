9d8d600beb79b62b3c37e2de8741baf7fa3ca31d
Madhava Reddy <90255418+Madhava-Reddy@users.noreply.github.com>
2025-02-02 15:34:27 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -69,6 +69,9 @@ module.exports = {
             '@root': path.resolve(__dirname, './'),
             'process/browser': require.resolve('process/browser'),
         },
+        fallback: {
+            "child_process": false,
+          }
     },
     module: {
         rules: [
