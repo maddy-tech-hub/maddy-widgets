757a1c6402f71c538fc375a33de45cf5c1cacd88
Madhava Reddy <90255418+Madhava-Reddy@users.noreply.github.com>
2025-02-08 22:34:45 +0530

    Update build.js

--- scripts/build.js
+++ scripts/build.js
@@ -2,7 +2,7 @@ const { execSync } = require('child_process');
 const environment = process.env.ENVIRONMENT || 'development';
 
 if (environment === 'production') {
-    execSync('export NODE_ENV=production && npx webpack --config webpack.config.js --mode production');
+    execSync('NODE_ENV=production npx webpack --config webpack.config.js --mode production', { stdio: 'inherit' });
 } else {
-    execSync('export NODE_ENV=development && npx webpack --config webpack.config.js --mode development');
+    execSync('NODE_ENV=development npx webpack --config webpack.config.js --mode development', { stdio: 'inherit' });
 }
